324b47ca71c383a7d8e24900b047f6e6e4e417a3
Magnus Bergroth <bergroth@nordu.net>
2017-11-06 13:09:14 +0000

    Adding new files

---
+++ includes/discovery/sensors/prefec_ber/junos.inc.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * LibreNMS
+ *
+ * Copyright (c) 2016 Neil Lathwood <neil@lathwood.co.uk>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.  Please see LICENSE.txt at the top level of
+ * the source code distribution for details.
+ *tis*/
+
+echo 'JunOS ';
+
+$multiplier = 1;
+$divisor    = 1;
+foreach ($pre_cache['junos_ifotn_oids'] as $index => $entry) {
+    if (is_numeric($entry['jnxoptIfOTNPMCurrentFECBERMantissa'])) {
+	$index = substr_replace($index,'',-2);
+        $oid = '.1.3.6.1.4.1.2636.3.73.1.3.3.8.1.5.'.$index.'.1';
+        #$descr = dbFetchCell('SELECT `ifDescr` FROM `ports` WHERE `ifIndex`= ? AND `device_id` = ?', array($index, $device['device_id'])) . ' electrical SNR';
+        $interface = dbFetchCell('SELECT `ifDescr` FROM `ports` WHERE `ifIndex`= ? AND `device_id` = ?', array($index, $device['device_id']));
+	$descr = $interface . ' preFEC BER';
+/*	# create ifoptioc_aternative index et-0/0/0 eq 1.1.1.1 
+	$t = explode('/',$interface,3);
+	$t0 = explode('-',$t[0],2);
+	$t1 = $t0[1] + 1;
+	$t2 = $t[1] + 1;
+	$t3 = $t[2] + 1;
+	$alt_index = '1.' . $t1 . '.' . $t2 . '.' . $t3;
+*/
+        #$limit_low = $pre_cache['junos_ifoptics2_oids'][$alt_index]['jnxCarFreqOffsetLowThresh']*$multiplier;
+        $limit_low = null;
+	$warn_limit_low = null;
+        #$limit = $pre_cache['junos_ifoptics2_oids'][$alt_index]['jnxCarFreqOffsetHighThresh']*$multiplier;
+	$limit = null;
+	$warn_limit = null;
+	$tmp_exp = $pre_cache['junos_ifotn_oids'][$index.".1"]['jnxoptIfOTNPMCurrentFECBERExponent'];
+        $current = ($entry['jnxoptIfOTNPMCurrentFECBERMantissa']/100)*pow(10,(-$tmp_exp));
+        $entPhysicalIndex = $index;
+        $entPhysicalIndex_measured = 'ports';
+        discover_sensor($valid['sensor'], 'prefec_ber', $device, $oid, $index, 'junos', $descr, $divisor, $multiplier, $limit_low, $warn_limit_low, $warn_limit, $limit, $current, 'snmp', $entPhysicalIndex, $entPhysicalIndex_measured);
+    }
+}
